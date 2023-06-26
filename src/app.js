b07714b79b4dae81bca1df38a40907c997b7e57c
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-06-26 17:40:31 -0300

    Versão com alguns bônus

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import  express  from "express";
+import express from "express";
 import cors from "cors";
 
 const app = express()
@@ -9,35 +9,35 @@ let tweets = [];
 
 let user = []
 
-app.post('/sign-up',(req,res)=>{
-	const {username,avatar}= req.body
-	if (!req.body || !username || !avatar){
+app.post('/sign-up', (req, res) => {
+	const { username, avatar } = req.body
+	if (!req.body || !username || !avatar) {
 		res.sendStatus(400)
 		return
 	}
-	if (typeof(username)!=='string' || typeof(avatar)!=='string'){
+	if (typeof (username) !== 'string' || typeof (avatar) !== 'string') {
 		res.status(400).send('Todos os campos são obrigatórios!');
 		return
 	}
 
-	const new_user = {username,avatar}
-	
+	const new_user = { username, avatar }
+
 	user.push(new_user)
 	res.status(201).send('ok');
 })
 
-app.post('/tweets',(req,res)=>{
-	const {username,tweet}=req.body;
+app.post('/tweets', (req, res) => {
+	const { username, tweet } = req.body;
 
-	if (user.length === 0){
+	if (user.length === 0) {
 		res.sendStatus(401);
 		return
 	}
-	if (!req.body || !username || !tweet){
+	if (!req.body || !username || !tweet) {
 		res.sendStatus(400)
 		return
 	}
-	if (typeof(username)!=='string' || typeof(tweet)!=='string'){
+	if (typeof (username) !== 'string' || typeof (tweet) !== 'string') {
 		res.status(400).send('Todos os campos são obrigatórios!');
 		return
 	}
@@ -50,33 +50,41 @@ app.post('/tweets',(req,res)=>{
 
 	tweets.push(new_tweet);
 	res.status(201).send('ok');
-	
+
 
 })
 
-app.get('/tweets',(req,res)=>{
-	tweets.forEach((tweet)=>{
-		const avatar = user.find((i)=>i.username===tweet.username)
-		tweet.avatar = avatar.avatar;
+app.get('/tweets', (req, res) => {
+	const { username,page } = req.query;
+
+	console.log(username)
+	if (username) {
+		const tweet = tweets.filter(
+			(i) => i.username===username
+		)
+		console.log(tweet)
+		tweet.forEach((j) => {
+			j.avatar = user.avatar
 		})
-	
-	while (tweets.length>10){
+		return res.send(tweet)
+	}
+
+	if (!page){
+		return res.sendStatus(400)
+	}
+
+	tweets.forEach((tweet) => {
+		const avatar = user.find((i) => i.username === tweet.username)
+		tweet.avatar = avatar.avatar;
+	})
+
+	while (tweets.length > 10) {
 		tweets.shift()
 	}
 
-    res.send(tweets)
+	res.send(tweets)
 })
 
-app.get('/tweets/:username',(req,res)=>{
-	const {username} = req.params;
-	const tweet = tweets.filter((i)=>{
-		i.username === username
-	})
-	tweet.forEach((j)=>{
-		j.avatar = user.avatar
-	})
-	res.send(tweet)
 
-})
 
-app.listen(5000,()=>console.log('Ta rodando na porta 5000'))
+app.listen(5000, () => console.log('Ta rodando na porta 5000'))
